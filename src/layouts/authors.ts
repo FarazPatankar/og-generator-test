56446d5c4333410018848946f791f8577c75b4a0
root <root@production-builder.us-west1-b.c.railway-infra.internal>
2021-06-21 10:00:19 +0000

    Initial Commit

---
+++ src/layouts/authors.ts
@@ -0,0 +1,26 @@
+export const authors = [
+  {
+    name: "Faraz Patankar",
+    image: "https://og.railway.app/authors/faraz-patankar.jpeg",
+  },
+  {
+    name: "Jake Cooper",
+    image: "https://og.railway.app/authors/jake-cooper.jpeg",
+  },
+  {
+    name: "Greg Schier",
+    image: "https://og.railway.app/authors/greg-schier.jpeg",
+  },
+  {
+    name: "Jake Runzer",
+    image: "https://og.railway.app/authors/jake-runzer.jpeg",
+  },
+];
+
+const defaultAuthor = authors[0];
+
+export const getAuthor = (name: string) => {
+  const author = authors.find(author => author.name === name);
+
+  return author ?? defaultAuthor;
+};
